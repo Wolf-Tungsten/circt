02a987c4da7f98551dd85aec4e9f2b3724521c1c
wolf-tungsten <gaoruihao@ict.ac.cn>
2025-09-23 16:54:53 +0800

    partially support strip external module

--- lib/Dialect/HW/Transforms/HWStripExternalModule.cpp
+++ lib/Dialect/HW/Transforms/HWStripExternalModule.cpp
@@ -42,11 +42,16 @@ struct HWStripExternalModule
   llvm::SmallVector<std::pair<std::string, Value>, 4> srcToOutputValues;
   llvm::SmallVector<std::pair<std::string, Value>, 4> srcToInputValues;
 
+  llvm::SmallVector<Operation *, 4> dstToErase;
+  llvm::SmallVector<std::pair<std::string, Value>, 4> dstToOutputValues;
+  llvm::SmallVector<std::pair<std::string, Value>, 4> dstToInputValues;
+
   uint64_t svOpLabelCount = 0;
   LogicalResult addLabelToSVOp();
   LogicalResult addLabelToSVOp(Operation *op, bool top);
   void processSrcSVOpRecursivly(Operation *op);
   LogicalResult processSrcHWModule();
+  void processDstSVOpRecursivly(Operation *op);
   LogicalResult processDstHWModule();
 
   bool isSVOp(Operation *op) {
@@ -226,11 +231,123 @@ void HWStripExternalModule::processSrcSVOpRecursivly(Operation *op) {
 };
 
 LogicalResult HWStripExternalModule::processDstHWModule() {
-  // // 遍历 dstHWModuleOp 的所有操作
-  // dstHWModuleOp.walk([&](Operation *op) {
-  //   if (isExternalOp(op)) {
-  //     op->erase();
-  //   }
-  // });
+  auto terminatorOp = dstHWModuleOp.getBody().front().getTerminator();
+  // step 1. 收集所有需要添加的接口、需要删除的 op
+  for (auto &block : dstHWModuleOp.getBody().getBlocks()) {
+    for (auto &op : block.getOperations()) {
+      if (isSVOp(&op)) {
+        processDstSVOpRecursivly(&op);
+      } else if (isExternalInstanceOp(&op)) {
+        // 处理输入，如果输入是来自 op 的，则添加成输入
+        auto instanceOp = cast<hw::InstanceOp>(op);
+        auto instanceName = instanceOp.getInstanceName().str();
+        for (unsigned int i = 0; i < instanceOp.getNumOperands(); i++) {
+          auto operand = instanceOp.getOperand(i);
+          std::string inputName = "extp_";
+          inputName += instanceName;
+          inputName += "_in_";
+          inputName += std::to_string(i);
+          dstToInputValues.push_back({inputName, operand});
+        }
+        // 处理输出，如果输出是被 op 使用的，则添加成输出
+        for (unsigned int i = 0; i < instanceOp.getNumResults(); i++) {
+          auto result = instanceOp.getResult(i);
+          std::string outputName = "extp_";
+          outputName += instanceName;
+          outputName += "_out_";
+          outputName += std::to_string(i);
+          dstToOutputValues.push_back({outputName, result});
+        }
+      } else {
+        // 其他操作除了 terminator 都删除
+        if (&op != dstHWModuleOp.getBody().front().getTerminator()) {
+          dstToErase.push_back(&op);
+        }
+      }
+    }
+  }
+  // step 2. 记录原有接口数量
+  unsigned int originalInputNum = dstHWModuleOp.getNumInputPorts();
+  unsigned int originalOutputNum = dstHWModuleOp.getNumOutputPorts();
+  llvm::SmallVector<unsigned, 4> inputIndicesToRemove;
+  llvm::SmallVector<unsigned, 4> outputIndicesToRemove;
+  for (unsigned i = 0; i < originalInputNum + originalOutputNum; i++) {
+    llvm::outs() << "Port " << i << ": ";
+    if (dstHWModuleOp.getPort(i).isInput()) {
+      llvm::outs() << "Input\n";
+      inputIndicesToRemove.push_back(i);
+    } else if (dstHWModuleOp.getPort(i).isOutput()) {
+      llvm::outs() << "Output\n";
+      outputIndicesToRemove.push_back(i);
+    }
+  }
+
+  // step 3. 将原有输出删除
+  auto outOp = cast<hw::OutputOp>(terminatorOp);
+  outOp->eraseOperands(0, originalOutputNum);
+  dstHWModuleOp.erasePorts({}, outputIndicesToRemove);
+
+  // step 3. 添加输入
+  // for (auto [name, value] : dstToInputValues) {
+  //   auto [portName, blockArgument] =
+  //       dstHWModuleOp.appendInput(name, value.getType());
+  //   value.replaceAllUsesWith(blockArgument);
+  // }
+
+  // step x. 删除所有标记的操作
+  // for (auto op : dstToErase) {
+  //   op->dropAllUses();
+  //   op->erase(); // 现在才能安全释放
+  // }
+
+  // for (auto [name, value] : dstToInputValues) {
+  //   auto [portName, blockArgument] =
+  //       dstHWModuleOp.appendInput(name, value.getType());
+  //   value.replaceAllUsesWith(blockArgument);
+  // }
+  // // 添加新的输出端口
+  // for (auto [name, value] : dstToOutputValues) {
+  //   dstHWModuleOp.appendOutput(name, value);
+  // }
+
+  // dstHWModuleOp.modifyPorts({}, {}, inputIndicesToRemove,
+  //                           outputIndicesToRemove);
+
+  // 修改 block arguments
+  // dstHWModuleOp.getBody().front().eraseArguments(0, originalInputNum);
+  // 修改 terminator operands
+  // terminatorOp->eraseOperands(0, originalOutputNum);
   return success();
 }
+
+void HWStripExternalModule::processDstSVOpRecursivly(Operation *op) {
+  // 分析 sv 操作的输入操作数
+  for (unsigned int i = 0; i < op->getNumOperands(); i++) {
+    auto operand = op->getOperand(i);
+
+    std::string inputName = "extp_sv";
+    inputName += getSVOpLabel(op);
+    inputName += "_in_";
+    inputName += std::to_string(i);
+    auto defOp = operand.getDefiningOp();
+    if (defOp == nullptr) {
+      // 这个 operand 是 block argument，需要判断是不是顶层模块的输入
+      if (operand.getParentBlock() == &dstHWModuleOp.getBody().front()) {
+        dstToInputValues.push_back({inputName, operand});
+      }
+    } else {
+      if (defOp->getParentOp() ==
+          dstHWModuleOp) { // 这个 operand 是定义在 dstHWModuleOp 层次上的
+        dstToInputValues.push_back({inputName, operand});
+      }
+    }
+  }
+
+  for (auto &region : op->getRegions()) {
+    for (auto &block : region) {
+      for (auto &nestedOp : block) {
+        processDstSVOpRecursivly(&nestedOp);
+      }
+    }
+  }
+};
